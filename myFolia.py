bf2a01c3b96558620e11e4f82923d83761caa670
samthetechie <samuelcarlisle@gmail.com>
2014-08-09 00:42:50 +0200

    adding some documentation and fixing shebang

--- myFolia.py
+++ myFolia.py
@@ -1,9 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+'''
+This is a python myFolia scraper with a cli wrapper.
+You can also use it directly as a Python module in your code.
+
+usage:
+
+ $ python myFolia.py carrot
+
+sample output:
+
+[[u'Category', u'Biennial'],
+ [u'Difficulty', u'Easy 2/5'],
+ [u'Lifecycle', u'Biennial'],
+ [u'Growth Habit', u'Erect'],
+ [u'Hardiness', u'Very Hardy'],
+ [u'Country of Origin', u'Afghanistan'],
+ [u'Mature Height', u'15.0 cm / 5.85 inches'],
+ [u'Mature Spread', u'2.5 cm / 0.98 inches'],
+ [u'USDA Zone Range',
+  u'Zone 3\n          \n          to\n          \n          Zone 11'],
+ [u'pH Range', u'6.0 - 6.5'],
+ [u'Water Requirements', u'High'],
+ [u'Nitrogen Requirements', u'Medium'],
+ [u'Soil', u'Loam'],
+ [u'Sun', u'Full Sun'],
+ [u'Can Sow Direct?', u'Yes'],
+ [u'Sowing Depth', u'0.6 cm / 0.23 inches'],
+ [u'Sowing Distance Apart', u'1.0 cm / 0.39 inches'],
+ [u'Sowing Row Distance Apart', u'Help build our wiki!'],
+ [u'Ideal Germination Temperature Range', u'16\xb0C / 61\xb0F'],
+ [u'Growing Temperatures', u'Help build our wiki!'],
+ [u'Planting Distance Apart', u'5.0 cm / 1.95 inches'],
+ [u'Planting Row Distance Apart', u'5.0 cm / 1.95 inches'],
+ [u'Bloom Time', u'Help build our wiki!'],
+ [u'Harvest Time', u'Late Spring'],
+ [u'Pruning Time', u'Late Spring']]
+'''
 
 import requests
 from bs4 import BeautifulSoup
-
-#from lxml import etree
 
 
 class ENotFound(Exception):
