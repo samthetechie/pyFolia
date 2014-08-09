b346c4eea28e8dca678b5913030e215ed2479d64
samthetechie <samuelcarlisle@gmail.com>
2014-08-09 13:42:34 +0200

    work in progress commit on city or country from address to USDA zone lookup script.

---
+++ usda.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+'''
+This is a python USDA growing zone lookup tool with a cli wrapper. It simple yet powerful command line address to USDA growing zone lookup.
+It has the following providers to choose from: OSM, Bing, Nokia, Yahoo, Google, ArcGIS, TomTom, Geonames, MapQuest, Geocoder.ca.
+You can also use it directly as a Python module in your code.
+
+usage:
+
+$ usda.py "Oranienstraße 184, 10999 Berlin, Germany"
+#D145, 47410 Saint-Colomb-de-Lauzun, France
+
+sample output: 
+
+7a
+'''
+
+import re
+import csv
+
+
+address = "Oranienstraße 184, 10999 Berlin, Germany"
+addressTokens = address.split(",")
+numTokens = len(addressTokens)
+city = re.sub(" \d+", " ", addressTokens[numTokens-2].lower()).strip() #'berlin'
+country = addressTokens[numTokens-1].lower().strip() #'germany'
+
+with open('usda.csv', 'rt') as f:
+     reader = csv.reader(f, delimiter=',')
+     for row in reader:
+     	#print row[1].lower() #country
+     	#print row[0].lower() #city
+     	    	
+     	if row[0].lower() == city:
+     		#print "match"
+     		print row[2].lower().strip()
+
+     	else:
+     		if row[1].lower() == country:
+				#print "match"
+     			print row[2].lower().strip()
+'''
