f6d9d9930af782d9eda26cdc2645c6c272627dfc
samthetechie <samuelcarlisle@gmail.com>
2014-08-15 06:46:52 +0200

    added some notes to help break the problem down

--- plantmonoculture.py
+++ plantmonoculture.py
@@ -10,7 +10,20 @@ usage:
 sample output:
 graphical display of hexagons with scale.
 
+notes: this does not have any complex algorithmic optimisation, just data -> geometric modifiers-> graph
 
-notes: this does not have any complex algorithmic optimisation, just data -> graph
+task 1: plot in a grid (square)
+input: grid_rows, grid_cols, "carrot"
+output: standard graph + "scale: " overlay
+
+task 2: plot on a plot (square)
+input: size_x, sixe_y, "carrot"
+output: standard graph + "scale: " overlay
+
+geomrtry question: how does the plant spacing value get changed when (square -> hex)?
+
+task 3: plot on a hexgrid (hex)
+input: hexgrid_rows, hexgrid_columns, "carrot"
+output: standard graph + "scale: " overlay
 
 '''
